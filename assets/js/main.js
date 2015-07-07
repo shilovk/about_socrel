b759022bba4e3ddfc1460c9776c8c9542730b164
shilovk <shilovk@shilovk.ru>
2015-07-07 15:15:49 +0400

    ok snoski

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,3 +217,14 @@
 	});
 
 })(jQuery);
+
+
+function image_show(id,show, img) {
+  if (show=="1") {
+    document.getElementById(id).style.visibility = "visible"
+    document.getElementById(id).childNodes[1].src = img;
+  }
+  else if (show=="0") {
+    document.getElementById(id).style.visibility = "hidden"
+  }
+}
